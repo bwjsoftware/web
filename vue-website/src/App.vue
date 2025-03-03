b7ca90870b914c3a130dd45f0bd6f8fade8bda15
Brett Johnson <johnson.brett@bwjsoftware.com>
2025-03-02 18:04:35 -0600

    Initial commit for creating Vuejs website

---
+++ vue-website/src/App.vue
@@ -0,0 +1,3 @@
+<template>
+	<h1 class='text-2xl'>Brett Johnson</h1>
+</template>
---
+++ vue-website/tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['sans-serif']
+			},
+			gridTemplateColumns: {
+				'70/30': '70% 28%'
+			},
+		},
+	},
+	plugins: [],
+}
