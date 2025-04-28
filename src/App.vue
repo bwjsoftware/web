a089912d105cb8e92813379848665d54776550bc
Brett Johnson <johnson.brett@bwjsoftware.com>
2025-04-28 18:37:33 -0500

    added darkmode

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,12 @@
+<script setup>
+	import { useDark } from '@vueuse/core';
+	const toggleDark = () => {document.documentElement.classList.toggle('dark');}
+
+</script>
+
 <template>
-	<h1 class='text-2xl'>Brett Johnson</h1>
+	<div class="flow-root">
+		<h1 class="text-4xl font-semibold text-black dark:text-white float-left m-[15px]">Welcome</h1>
+		<button class="text-xl bg-black dark:bg-white dark:text-black text-white float-right m-[15px] rounded"  @click="toggleDark">Toggle Darkmode</button>
+	</div>
 </template>
