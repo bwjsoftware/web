30637affb22b808d0da07a4076eb8e0f30ddefef
Brett Johnson <johnson.brett@bwjsoftware.com>
2025-05-03 16:42:29 -0500

    reworked how images are handled they now have proper transparent backgrounds -- Need to find a better background color for dark mode

--- src/App.vue
+++ src/App.vue
@@ -16,12 +16,11 @@
 <template>
 	<div class="flow-root">
 		<h1 class="text-4xl font-semibold text-black dark:text-white float-left m-[15px]">Welcome</h1>
-		<img class="float-right m-[15px]" src="./assets/img/Logo.png" alt="bwjsoftware logo" width="225">
+		<img class="float-right m-[15px] bg-black dark:bg-transparent rounded-2xl" src="./assets/img/Logo-no-bg.png" alt="bwjsoftware logo" width="225">
 	</div>
 	<div class="flow-root ">
 		<a href="https://github.com/brett092304" target="_blank" rel="noopener noreferrer">
-			<img v-if="isDarkMode" class="float-left m-[15px]" src="./assets/img/github-dark-logo.png" alt="github logo" width="100">
-			<img v-else class="float-left m-[15px]" src="./assets/img/github-light-logo.png" alt="github logo" width="100">
+			<img :src="isDarkMode ? '/img/github-dark-logo.png' : '/img/github-light-logo.png'" :alt="isDarkMode ? 'github logo dark' : 'github logo light'" class="float-left m-[15px]" width="150" />
 		</a>
 	</div>
 
