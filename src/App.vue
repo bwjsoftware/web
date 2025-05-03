f0540359cb2e7f873c62ee7337d5c75c30c7f5a7
Brett Johnson <johnson.brett@bwjsoftware.com>
2025-05-02 22:14:57 -0500

    Working on making it so dark mode respects browser settings

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,9 @@
 <script setup>
-	import { useDark } from '@vueuse/core';
-	const toggleDark = () => {document.documentElement.classList.toggle('dark');}
 
 </script>
 
 <template>
 	<div class="flow-root">
 		<h1 class="text-4xl font-semibold text-black dark:text-white float-left m-[15px]">Welcome</h1>
-		<button class="text-xl bg-black dark:bg-white dark:text-black text-white float-right m-[15px] rounded"  @click="toggleDark">Toggle Darkmode</button>
 	</div>
 </template>
