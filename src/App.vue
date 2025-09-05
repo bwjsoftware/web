6ac126dd151642717c7aa900235a810a88eea5a6
Brett Johnson <johnson.brett@bwjsoftware.com>
2025-09-05 08:58:31 -0500

    Started adding proper colors for background in dark mode

--- src/App.vue
+++ src/App.vue
@@ -15,11 +15,11 @@
 
 <template>
 	<div class="flow-root">
-		<h1 class="text-4xl font-semibold text-black dark:text-white float-left m-[15px]">Welcome</h1>
+		<h1 class="text-4xl font-semibold text-black dark:text-fg float-left m-[15px]">Welcome</h1>
 		<img class="float-right m-[15px] bg-black dark:bg-transparent rounded-2xl" src="./assets/img/Logo-no-bg.png" alt="bwjsoftware logo" width="225">
 	</div>
 	<div class="flow-root ">
-		<a href="https://github.com/brett092304" target="_blank" rel="noopener noreferrer">
+		<a href="https://github.com/bwjsoftware" target="_blank" rel="noopener noreferrer">
 			<img :src="isDarkMode ? '/img/github-dark-logo.png' : '/img/github-light-logo.png'" :alt="isDarkMode ? 'github logo dark' : 'github logo light'" class="float-left m-[15px]" width="150" />
 		</a>
 	</div>
